71acb58b77991d4e0701cc01e3e7aef15ed0a96c
Alireza Kenarsari <alireza@picovoice.ai>
2019-01-01 23:49:36 -0800

    builds

---
+++ demo/android/RhinoDemoWithPorcupine/app/src/main/java/ai/picovoice/rhinodemowithporcupine/RhinoDemoWithPorcupineActivity.java
@@ -0,0 +1,13 @@
+package ai.picovoice.rhinodemowithporcupine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RhinoDemoWithPorcupineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rhino_demo_with_porcupine);
+    }
+}
