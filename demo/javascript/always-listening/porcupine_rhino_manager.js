f18c9d9dd71074553271bd54f5cbb77718a12a00
David Bartle <dbartle@gmail.com>
2020-06-04 19:21:16 -0700

    Format porcupine_rhino_manager

--- demo/javascript/always-listening/porcupine_rhino_manager.js
+++ demo/javascript/always-listening/porcupine_rhino_manager.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2018 Picovoice Inc.
+    Copyright 2018-2020 Picovoice Inc.
     You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
     file accompanying this source.
     Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
@@ -7,54 +7,65 @@
     specific language governing permissions and limitations under the License.
 */
 
-PorcupineRhinoManager = function (porcupineWorkerScript, rhinoWorkerScript, downsamplingScript) {
-    let porcupineWorker;
-    let rhinoWorker;
+PorcupineRhinoManager = function (
+  porcupineWorkerScript,
+  rhinoWorkerScript,
+  downsamplingScript
+) {
+  let porcupineWorker;
+  let rhinoWorker;
 
-    let isWakeWordDetected = false;
+  let isWakeWordDetected = false;
 
-    let start = function (keywordsID, keywordSensitivities, keywordDetectionCallback, context, inferenceCallback, errorCallback) {
-        porcupineWorker = new Worker(porcupineWorkerScript);
-        porcupineWorker.postMessage({
-            command: "init",
-            keywordIDs: keywordsID,
-            sensitivities: keywordSensitivities
-        });
+  let start = function (
+    keywordsID,
+    keywordSensitivities,
+    keywordDetectionCallback,
+    context,
+    inferenceCallback,
+    errorCallback
+  ) {
+    porcupineWorker = new Worker(porcupineWorkerScript);
+    porcupineWorker.postMessage({
+      command: "init",
+      keywordIDs: keywordsID,
+      sensitivities: keywordSensitivities,
+    });
 
-        rhinoWorker = new Worker(rhinoWorkerScript);
-        rhinoWorker.postMessage({command: "init", context: context});
+    rhinoWorker = new Worker(rhinoWorkerScript);
+    rhinoWorker.postMessage({ command: "init", context: context });
 
-        porcupineWorker.onmessage = function (e) {
-            if (!isWakeWordDetected) {
-                isWakeWordDetected = e.data.keyword !== null;
+    porcupineWorker.onmessage = function (e) {
+      if (!isWakeWordDetected) {
+        isWakeWordDetected = e.data.keyword !== null;
 
-                if (isWakeWordDetected) {
-                    keywordDetectionCallback(e.data.keyword);
-                }
-            }
-        };
-
-        rhinoWorker.onmessage = function (e) {
-            inferenceCallback(e.data);
-            isWakeWordDetected = false;
-        };
-
-        WebVoiceProcessor.start([this], downsamplingScript, errorCallback);
-    };
-
-    let stop = function () {
-        WebVoiceProcessor.stop();
-        porcupineWorker.postMessage({command: "release"});
-        rhinoWorker.postMessage({command: 'release'});
-    };
-
-    let processFrame = function (frame) {
-        if (!isWakeWordDetected) {
-            porcupineWorker.postMessage({command: "process", inputFrame: frame});
-        } else {
-            rhinoWorker.postMessage({command: "process", inputFrame: frame});
+        if (isWakeWordDetected) {
+          keywordDetectionCallback(e.data.keyword);
         }
+      }
     };
 
-    return {start: start, processFrame: processFrame, stop: stop}
+    rhinoWorker.onmessage = function (e) {
+      inferenceCallback(e.data);
+      isWakeWordDetected = false;
+    };
+
+    WebVoiceProcessor.start([this], downsamplingScript, errorCallback);
+  };
+
+  let stop = function () {
+    WebVoiceProcessor.stop();
+    porcupineWorker.postMessage({ command: "release" });
+    rhinoWorker.postMessage({ command: "release" });
+  };
+
+  let processFrame = function (frame) {
+    if (!isWakeWordDetected) {
+      porcupineWorker.postMessage({ command: "process", inputFrame: frame });
+    } else {
+      rhinoWorker.postMessage({ command: "process", inputFrame: frame });
+    }
+  };
+
+  return { start: start, processFrame: processFrame, stop: stop };
 };
