32246548b54f0bfcffabe097b82da8e9609e1241
millasml <millasml@yahoo.com.sg>
2020-06-18 22:11:41 +0800

    added progress indicators

--- demo/javascript/smart-lighting/src/App.js
+++ demo/javascript/smart-lighting/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Picovoice from "./picovoice/picovoice"
-import "@picovoice/web-voice-processor/src/web_voice_processor"
+import Picovoice from "./picovoice/picovoice";
+import "@picovoice/web-voice-processor/src/web_voice_processor";
 import "./App.css";
 
-const demo = new Picovoice()
-
+const demo = new Picovoice();
 
 const LIVING_ROOM = "living room";
 const BEDROOM = "bedroom";
@@ -293,7 +292,7 @@ export default function LightingDemo(props) {
   };
 
   const startListening = () => {
-    demo.start(initEvent, keywordEvent, inferenceEvent)
+    demo.start(initEvent, keywordEvent, inferenceEvent);
     setListening(true);
     setWakePhrase(false);
     setIntentFailed(false);
@@ -317,54 +316,54 @@ export default function LightingDemo(props) {
     setMessage("");
   };
 
-  const inferenceEvent = information => {
-    setWakePhrase(false)
+  const inferenceEvent = (information) => {
+    setWakePhrase(false);
 
     if (information.isUnderstood === false) {
-      setIntentFailed(true)
+      setIntentFailed(true);
     } else {
-      setIntentFailed(false)
-      const intent = information.intent
-      const slots = information.slots
+      setIntentFailed(false);
+      const intent = information.intent;
+      const slots = information.slots;
       const location =
-        slots["location"] === undefined ? "all" : slots["location"]
+        slots["location"] === undefined ? "all" : slots["location"];
 
       if (intent === "changeLightState") {
-        const state = slots["state"] === "on" ? true : false
-        STATE_MAP.get(location)(state)
+        const state = slots["state"] === "on" ? true : false;
+        STATE_MAP.get(location)(state);
       } else if (intent === "changeLightStateOff") {
-        STATE_MAP.get(location)(false)
+        STATE_MAP.get(location)(false);
       } else if (intent === "changeColor") {
-        const color = LIGHT_MAP.get(slots["color"])
-        COLOR_MAP.get(location)(color)
+        const color = LIGHT_MAP.get(slots["color"]);
+        COLOR_MAP.get(location)(color);
         if (location !== "all") {
-          STATE_MAP.get(location)(true)
+          STATE_MAP.get(location)(true);
         }
       } else if (intent === "changeIntensity") {
-        const dir = DIRECTION_MAP.get(slots["intensity"])
-        changeIntensity(location, dir)
+        const dir = DIRECTION_MAP.get(slots["intensity"]);
+        changeIntensity(location, dir);
       } else if (intent === "reset") {
-        const resetAspect = RESET_MAP.get(slots["feature"])
+        const resetAspect = RESET_MAP.get(slots["feature"]);
         if (location === null || location === undefined) {
           if (resetAspect === ASPECT_COLOR) {
-            setAllLightColor(LIGHT_YELLOW)
+            setAllLightColor(LIGHT_YELLOW);
           } else if (resetAspect === ASPECT_INTENSITY) {
-            setAllLightIntensity(1)
+            setAllLightIntensity(1);
           }
         } else {
           if (resetAspect === ASPECT_COLOR) {
-            COLOR_MAP.get(location)(LIGHT_YELLOW)
+            COLOR_MAP.get(location)(LIGHT_YELLOW);
           } else if (resetAspect === ASPECT_INTENSITY) {
-            changeIntensity(location, "reset")
+            changeIntensity(location, "reset");
           }
         }
       }
     }
-  }
+  };
 
   useEffect(() => {
-    demo.refresh(initEvent, keywordEvent, inferenceEvent)
-  })
+    demo.refresh(initEvent, keywordEvent, inferenceEvent);
+  });
 
   useEffect(() => {
     if (message !== "") {
@@ -382,7 +381,6 @@ export default function LightingDemo(props) {
       } catch (exception) {}
     };
   }, []);
-
 
   return (
     <>
@@ -954,6 +952,26 @@ export default function LightingDemo(props) {
                 KITCHEN
               </text>
             </svg>
+          </div>
+          <div className="progress-indicator">
+            {demoLoading && (
+              <>
+                <div className="spinner initializing"></div>
+                <div>initializing Demo...</div>
+              </>
+            )}
+            {listening && !demoLoading && !wakePhrase && (
+              <>
+                <div className="spinner wakephrase"></div>
+                <div>Listening for "Pico House" or "Pico Home"...</div>
+              </>
+            )}
+            {listening && !demoLoading && wakePhrase && (
+              <>
+                <div className="spinner command"></div>
+                <div>Listening for command...</div>
+              </>
+            )}
           </div>
           <button
             disabled={demoLoading}
