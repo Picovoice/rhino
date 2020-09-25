529b133cbea5acc156dec0f6bf9aa926cc17019c
Alireza Kenarsari <alireza@picovoice.ai>
2020-09-24 17:32:35 -0700

    oops

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from .binding.python.rhino import Rhino
 from .resources.util.python import *
 
 
-def create(library_path=None, model_path=None, context_path=None, context=None):
+def create(library_path=None, model_path=None, context_path=None):
     """
     Factory method for Rhino
 
@@ -10,7 +10,6 @@ def create(library_path=None, model_path=None, context_path=None, context=None):
     :param model_path: Absolute path to file containing model parameters.
     :param context_path: Absolute path to file containing context parameters. A context represents the set of
     expressions (spoken commands), intents, and intent arguments (slots) within a domain of interest.
-    :param context: The context to be used by Rhino. List of default contexts can be retrieved via 'pvrhino.CONTEXTS'
     :return: An instance of Rhino Speech-to-Intent engine.
     """
 
@@ -19,13 +18,5 @@ def create(library_path=None, model_path=None, context_path=None, context=None):
 
     if model_path is None:
         model_path = MODEL_PATH
-
-    if context_path is None:
-        if context is None:
-            raise ValueError("'context' or 'context_path' must be set")
-        if context not in CONTEXTS:
-            raise ValueError(
-                "context '%s' is not available by default. default contexts are :\n%s" % (context, ', '.join(CONTEXTS)))
-        context_path = CONTEXT_PATHS[context]
 
     return Rhino(library_path=library_path, model_path=model_path, context_path=context_path)
