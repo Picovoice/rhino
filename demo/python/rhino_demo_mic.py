1abd6ffde0bb61ec90b9f6c800cf9cb25ff03dfd
Alireza Kenarsari <alireza@picovoice.ai>
2020-10-02 11:35:35 -0700

    refactoring demos

--- demo/python/rhino_demo_mic.py
+++ demo/python/rhino_demo_mic.py
@@ -22,20 +22,21 @@ import soundfile
 
 class RhinoDemo(Thread):
     """
-    Demo class for Speech-to-Intent (aka Rhino) library. It creates an input audio stream from a microphone, monitors
-    it, and upon detecting the specified wake phrase extracts the intent from the speech command that follows. Wake word
-    detection is done using Porcupine's wake word detection engine (https://github.com/Picovoice/Porcupine).
+    Microphone Demo for Rhino Speech-to-Intent engine. It creates an input audio stream from a microphone, monitors
+    it, and extracts the intent from the speech command. It optionally saves the recorded audio into a file for further
+    debugging.
     """
 
-    def __init__(self, library_path, model_path, context_path, input_device_index=None, output_path=None):
+    def __init__(self, library_path, model_path, context_path, audio_device_index=None, output_path=None):
         """
         Constructor.
 
-        :param library_path: Absolute path to dynamic library.
-        :param model_path: Absolute path to model file.
-        :param context_path: Absolute path to context file.
-        :param input_device_index: If provided, audio is recorded from this input device. Otherwise, the default audio
-        input device is used.
+        :param library_path: Absolute path to Rhino's dynamic library.
+        :param model_path: Absolute path to file containing model parameters.
+        :param context_path: Absolute path to file containing context parameters. A context represents the set of
+        expressions (spoken commands), intents, and intent arguments (slots) within a domain of interest.
+        :param audio_device_index: Optional argument. If provided, audio is recorded from this input device. Otherwise,
+        the default audio input device is used.
         :param output_path: If provided recorded audio will be stored in this location at the end of the run.
         """
 
@@ -45,7 +46,7 @@ class RhinoDemo(Thread):
         self._model_path = model_path
         self._context_path = context_path
 
-        self._input_device_index = input_device_index
+        self._audio_device_index = audio_device_index
 
         self._output_path = output_path
         if self._output_path is not None:
@@ -53,9 +54,8 @@ class RhinoDemo(Thread):
 
     def run(self):
         """
-         Creates an input audio stream, initializes wake word detection (Porcupine) and speech to intent (Rhino)
-         engines, and monitors the audio stream for occurrences of the wake word and then infers the intent from speech
-         command that follows.
+         Creates an input audio stream, instantiates an instance of Rhino object, and infers the intent from spoken
+         commands.
          """
 
         rhino = None
@@ -68,12 +68,6 @@ class RhinoDemo(Thread):
                 model_path=self._model_path,
                 context_path=self._context_path)
 
-            print()
-            print('****************************** context ******************************')
-            print(rhino.context_info)
-            print('*********************************************************************')
-            print()
-
             pa = pyaudio.PyAudio()
 
             audio_stream = pa.open(
@@ -82,7 +76,10 @@ class RhinoDemo(Thread):
                 format=pyaudio.paInt16,
                 input=True,
                 frames_per_buffer=rhino.frame_length,
-                input_device_index=self._input_device_index)
+                input_device_index=self._audio_device_index)
+
+            print(rhino.context_info)
+            print()
 
             while True:
                 pcm = audio_stream.read(rhino.frame_length)
@@ -93,22 +90,20 @@ class RhinoDemo(Thread):
 
                 is_finalized = rhino.process(pcm)
                 if is_finalized:
-                    if rhino.is_understood():
-                        intent, slot_values = rhino.get_intent()
+                    inference = rhino.get_inference()
+                    if inference.is_understood:
                         print('{')
-                        print("  intent : '%s'" % intent)
+                        print("  intent : '%s'" % inference.intent)
                         print('  slots : {')
-                        for slot, value in slot_values.items():
+                        for slot, value in inference.slots.items():
                             print("    %s : '%s'" % (slot, value))
                         print('  }')
-                        print('}')
+                        print('}\n')
                     else:
-                        print("didn't understand the command")
-
-                    rhino.reset()
+                        print("Didn't understand the command.\n")
 
         except KeyboardInterrupt:
-            print('stopping ...')
+            print('Stopping ...')
 
         finally:
             if audio_stream is not None:
@@ -128,17 +123,15 @@ class RhinoDemo(Thread):
                     samplerate=rhino.sample_rate,
                     subtype='PCM_16')
 
-    _AUDIO_DEVICE_INFO_KEYS = ['index', 'name', 'defaultSampleRate', 'maxInputChannels']
-
     @classmethod
-    def show_audio_devices_info(cls):
-        """ Provides information regarding different audio devices available. """
+    def show_audio_devices(cls):
+        fields = ('index', 'name', 'defaultSampleRate', 'maxInputChannels')
 
         pa = pyaudio.PyAudio()
 
         for i in range(pa.get_device_count()):
             info = pa.get_device_info_by_index(i)
-            print(', '.join("'%s': '%s'" % (k, str(info[k])) for k in cls._AUDIO_DEVICE_INFO_KEYS))
+            print(', '.join("'%s': '%s'" % (k, str(info[k])) for k in fields))
 
         pa.terminate()
 
@@ -146,34 +139,34 @@ class RhinoDemo(Thread):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--library_path', default=pvrhino.LIBRARY_PATH, help="absolute path to dynamic library")
+    parser.add_argument('--context_path', help="Absolute path to context file.")
 
-    parser.add_argument('--model_path', default=pvrhino.MODEL_PATH, help="absolute path to model file")
-
-    parser.add_argument('--context_path', help="absolute path to context file")
-
-    parser.add_argument('--input_device_index', help='index of input audio device', type=int, default=None)
+    parser.add_argument('--library_path', help="Absolute path to dynamic library.", default=pvrhino.LIBRARY_PATH)
 
     parser.add_argument(
-        '--output_path',
-        help='absolute path to where recorded audio will be stored. If not set, it will be bypassed.',
-        default=None)
+        '--model_path',
+        help="Absolute path to the file containing model parameters.",
+        default=pvrhino.MODEL_PATH)
 
-    parser.add_argument('--show_audio_devices_info', action='store_true')
+    parser.add_argument('--audio_device_index', help='Index of input audio device.', type=int, default=None)
+
+    parser.add_argument('--output_path', help='Absolute path to recorded audio for debugging.', default=None)
+
+    parser.add_argument('--show_audio_devices', action='store_true')
 
     args = parser.parse_args()
 
-    if args.show_audio_devices_info:
-        RhinoDemo.show_audio_devices_info()
+    if args.show_audio_devices:
+        RhinoDemo.show_audio_devices()
     else:
         if not args.context_path:
-            raise ValueError('missing path to rhino context file')
+            raise ValueError('Missing path to context file')
 
         RhinoDemo(
             library_path=args.library_path,
             model_path=args.model_path,
             context_path=args.context_path,
-            input_device_index=args.input_device_index,
+            audio_device_index=args.audio_device_index,
             output_path=args.output_path).run()
 
 
