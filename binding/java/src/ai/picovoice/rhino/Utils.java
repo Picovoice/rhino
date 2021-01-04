e7c47baf1d7dc30de48e4387fad2464ebf18f6e5
Ian Lavery <ian@picovoice.ai>
2021-01-04 15:51:47 -0800

    changed java binding resource extraction to overwrite

--- binding/java/src/ai/picovoice/rhino/Utils.java
+++ binding/java/src/ai/picovoice/rhino/Utils.java
@@ -108,14 +108,11 @@ class Utils {
                         Files.createDirectory(dstPath);
                 } else {
                     Path file = resourceDirectoryPath.resolve(jarEntryName);
-                    if (!Files.exists(file)) {
-                        try (InputStream is = jf.getInputStream(jarEntry)) {
-                            Files.copy(is, file, StandardCopyOption.REPLACE_EXISTING);
-                        }
+                    try (InputStream is = jf.getInputStream(jarEntry)) {
+                        Files.copy(is, file, StandardCopyOption.REPLACE_EXISTING);
                     }
                 }
             }
-
         }
 
         return resourceDirectoryPath;
