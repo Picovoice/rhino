f8586a7b044bb2a266c8d16d77ba169a335f78dc
Ian Lavery <ian@picovoice.ai>
2021-01-07 15:44:57 -0800

    working on android

---
+++ demo/flutter/README.md
@@ -0,0 +1,72 @@
+# Rhino Flutter Demo
+
+To run the Rhino demo on Android or iOS with Flutter, you must have the [Flutter SDK](https://flutter.dev/docs/get-started/install) installed on your system. Once installed, you can run `flutter doctor` to determine any other missing requirements for your relevant platform. Once your environment has been set up, launch a simulator or connect an Android/iOS device. 
+
+## Usage
+
+Before launching the app, use the copy_assets.sh script to copy the rhino demo context file into the demo project. (**NOTE**: on Windows, Git Bash or another bash shell is required, or you will have to manually copy the context into the project.).
+
+Run the following command from [demo/flutter](/demo/flutter/) to build and deploy the demo to your device:
+```sh
+flutter run
+```
+
+The default context for this demo is `Smart Lighting`. Simply press start and the engine can recognize commands such as
+
+> Turn off the lights.
+
+or
+
+> Set the lights in the bedroom to blue.
+
+See below for the full context:
+
+```yaml
+context:
+  expressions:
+    changeColor:
+      - "[turn, make] (all, the) lights $color:color"
+      - "[change, set, switch] (all, the) lights to $color:color"
+      - "[turn, make] (the) $location:location (color, light, lights) $color:color"
+      - "[change, set, switch] (the) $location:location (color, light, lights) to $color:color"
+      - "[turn, make] (the) [color, light, lights] [at, in] (the) $location:location $color:color"
+      - "[change, set, switch] (the) [color, light, lights] [at, in] (the) $location:location to $color:color"
+      - "[turn, make] (the) [color, light, lights] $color:color [at, in] (the) $location:location"
+      - "[change, set, switch] (the) [color, light, lights] to $color:color [at, in] (the) $location:location"
+    changeLightState:
+      - "[switch, turn] $state:state (all, the) lights"
+      - "[switch, turn] (all, the) lights $state:state"
+      - "[switch, turn] $state:state (the) $location:location (light, lights)"
+      - "[switch, turn] (the) $location:location [light, lights] $state:state"
+      - "[switch, turn] $state:state (the) [light, lights] [at, in] (the) $location:location"
+      - "[switch, turn] (the) [light, lights] [in, at] the $location:location $state:state"
+    changeLightStateOff:
+      - "shut off (all, the) lights"
+      - "shut (all, the) lights off"
+      - "shut off (the) $location:location (light, lights)"
+      - "shut (the) $location:location (light, lights) off"
+      - "shut off (the) [light, lights] [at, in] (the) $location:location"
+      - "shut (the) [light, lights] off [at, in] (the) $location:location"
+      - "shut (the) [light, lights] [at, in] (the) $location:location off"
+  slots:
+    color:
+      - "blue"
+      - "green"
+      - "orange"
+      - "pink"
+      - "purple"
+      - "red"
+      - "white"
+      - "yellow"
+    state:
+      - "off"
+      - "on"
+    location:
+      - "bathroom"
+      - "bedroom"
+      - "closet"
+      - "hallway"
+      - "kitchen"
+      - "living room"
+      - "pantry"
+```
---
+++ demo/flutter/lib/main.dart
@@ -0,0 +1,187 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+import 'package:path_provider/path_provider.dart';
+import 'package:rhino/rhino_manager.dart';
+import 'package:rhino/rhino_error.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool isButtonDisabled = false;
+  bool isProcessing = false;
+  String rhinoText = "";
+  RhinoManager _rhinoManager;
+
+  @override
+  void initState() {
+    super.initState();
+    this.setState(() {
+      isButtonDisabled = true;
+      rhinoText = "";
+    });
+
+    initRhino();
+  }
+
+  Future<void> initRhino() async {
+    String contextPath = await _extractDemoContext();
+
+    try {
+      _rhinoManager = await RhinoManager.create(contextPath, inferenceCallback,
+          errorCallback: errorCallback);
+    } on PvError catch (ex) {
+      print("Failed to initialize Rhino: ${ex.message}");
+    } finally {
+      this.setState(() {
+        isButtonDisabled = false;
+      });
+    }
+  }
+
+  void inferenceCallback(Map<String, dynamic> inference) {
+    this.setState(() {
+      rhinoText = prettyPrintInference(inference);
+      isButtonDisabled = false;
+      isProcessing = false;
+    });
+  }
+
+  void errorCallback(PvError error) {
+    print(error.message);
+  }
+
+  String prettyPrintInference(Map<String, dynamic> inference) {
+    String printText =
+        "{\n    \"isUnderstood\" : \"${inference['isUnderstood']}\",\n";
+    if (inference['isUnderstood']) {
+      printText += "    \"intent\" : \"${inference['intent']}\",\n";
+      if (inference['slots'].length > 0) {
+        printText += '    "slots" : {\n';
+        Map<String, String> slots = inference['slots'];
+        for (String key in slots.keys) {
+          printText += "        \"$key\" : \"${slots[key]}\",\n";
+        }
+        printText += '    }\n';
+      }
+    }
+    printText += '}';
+    return printText;
+  }
+
+  Future<String> _extractDemoContext() async {
+    // file to extract
+    String platform = Platform.isAndroid ? "android" : "ios";
+    String filePath = "assets/contexts/$platform/smart_lighting_$platform.rhn";
+
+    // extraction destination
+    String resourceDirectory = (await getApplicationDocumentsDirectory()).path;
+    String outputPath = '$resourceDirectory/$filePath';
+    File outputFile = new File(outputPath);
+
+    ByteData data = await rootBundle.load(filePath);
+    final buffer = data.buffer;
+
+    await outputFile.create(recursive: true);
+    await outputFile.writeAsBytes(
+        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+    return outputPath;
+  }
+
+  Future<void> _startProcessing() async {
+    if (isProcessing) {
+      return;
+    }
+
+    this.setState(() {
+      isButtonDisabled = true;
+    });
+
+    try {
+      await _rhinoManager.process();
+      this.setState(() {
+        isProcessing = true;
+        rhinoText = "Listening...";
+      });
+    } on PvAudioException catch (ex) {
+      print("Failed to start audio capture: ${ex.message}");
+      this.setState(() {
+        isButtonDisabled = false;
+      });
+    }
+  }
+
+  Color picoBlue = Color.fromRGBO(55, 125, 255, 1);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Rhino Demo'),
+          backgroundColor: picoBlue,
+        ),
+        body: Column(
+          children: [
+            buildStartButton(context),
+            buildRhinoTextArea(context),
+            footer
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildStartButton(BuildContext context) {
+    return new Expanded(
+      flex: 2,
+      child: Container(
+          child: SizedBox(
+              width: 150,
+              height: 150,
+              child: RaisedButton(
+                shape: CircleBorder(),
+                textColor: Colors.white,
+                color: picoBlue,
+                onPressed: isButtonDisabled ? null : _startProcessing,
+                child: Text(isProcessing ? "..." : "Start",
+                    style: TextStyle(fontSize: 30)),
+              ))),
+    );
+  }
+
+  buildRhinoTextArea(BuildContext context) {
+    return new Expanded(
+        flex: 4,
+        child: Container(
+            alignment: Alignment.center,
+            color: Color(0xff25187e),
+            margin: EdgeInsets.all(20),
+            child: Text(
+              rhinoText,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            )));
+  }
+
+  Widget footer = Expanded(
+      flex: 1,
+      child: Container(
+          alignment: Alignment.bottomCenter,
+          padding: EdgeInsets.only(bottom: 20),
+          child: const Text(
+            "Made in Vancouver, Canada by Picovoice",
+            style: TextStyle(color: Color(0xff666666)),
+          )));
+}
