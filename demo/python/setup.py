cc44ebb76e52008e88861bcde6baad55a1dd3654
Alireza Kenarsari <alireza@picovoice.ai>
2021-02-26 11:00:54 -0800

    updated pip packages

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvrhinodemo",
-    version="1.6.1",
+    version="1.6.2",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Rhino Speech-to-Intent engine demos.",
@@ -36,7 +36,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/rhino",
     packages=["pvrhinodemo"],
-    install_requires=["enum34", "numpy", "pvrhino==1.6.0", "pyaudio", "soundfile"],
+    install_requires=["enum34", "numpy", "pvrhino==1.6.1", "pyaudio", "soundfile"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
