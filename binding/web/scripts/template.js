fcc98558241b6a44c0d33225164a3118b0648538
David Bartle <dbartle@picovoice.ai>
2021-04-08 14:27:24 -0700

    Update web build script to ESM

--- binding/web/scripts/template.js
+++ binding/web/scripts/template.js
@@ -1,6 +1,9 @@
-const ncp = require("ncp");
-const path = require("path");
-const editJsonFile = require("edit-json-file");
+import ncp from "ncp";
+import { dirname, join } from "path";
+import editJsonFile from "edit-json-file";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 for (const language of ["en", "de", "es", "fr"]) {
   for (const flavour of ["factory", "worker"]) {
@@ -9,50 +12,46 @@ for (const language of ["en", "de", "es", "fr"]) {
     // Workers
     // 1. Copy language-agnostic project: i.e., the 'template' folder, to target
     // (e.g. rhino-web-en-worker)
-    const projectRootPath = path.join(__dirname, "..");
-    const templateDirectory = path.join(projectRootPath, "template");
+    const projectRootPath = join(__dirname, "..");
+    const templateDirectory = join(projectRootPath, "template");
     const buildTarget = `rhino-web-${language}-${flavour}`;
-    const outputDirectory = path.join(projectRootPath, buildTarget);
+    const outputDirectory = join(projectRootPath, buildTarget);
     ncp(templateDirectory, outputDirectory, (err) => {
       console.log(`${language}: ncp done`);
       if (err) {
         console.error(err);
       } else {
         // 2. Copy language-specific features (Emscripten)
-        const languageDirectory = path.join(projectRootPath, language);
-        ncp(
-          languageDirectory,
-          path.join(outputDirectory, "src", "lang"),
-          (err) => {
-            if (err) {
-              console.error(error);
-            } else {
-              // 3. index.ts: Rollup's entry point is different for workers/factories
-              console.log(path.join(projectRootPath, flavour, "index.ts"));
-              console.log(path.join(outputDirectory, "src"));
-              ncp(
-                path.join(projectRootPath, flavour, "index.ts"),
-                path.join(outputDirectory, "src", "index.ts"),
-                (err) => {
-                  if (err) {
-                    console.error(error);
-                  } else {
-                    console.log("index.ts copied");
+        const languageDirectory = join(projectRootPath, language);
+        ncp(languageDirectory, join(outputDirectory, "src", "lang"), (err) => {
+          if (err) {
+            console.error(error);
+          } else {
+            // 3. index.ts: Rollup's entry point is different for workers/factories
+            console.log(join(projectRootPath, flavour, "index.ts"));
+            console.log(join(outputDirectory, "src"));
+            ncp(
+              join(projectRootPath, flavour, "index.ts"),
+              join(outputDirectory, "src", "index.ts"),
+              (err) => {
+                if (err) {
+                  console.error(error);
+                } else {
+                  console.log("index.ts copied");
 
-                    // 4. Customize the package.json to have the correct names and build targets
-                    const packageJson = editJsonFile(
-                      path.join(outputDirectory, "package.json")
-                    );
-                    packageJson.set("name", `@picovoice/${buildTarget}`);
-                    packageJson.save((e) => {
-                      console.log(`${buildTarget} Package JSON updated`);
-                    });
-                  }
+                  // 4. Customize the package.json to have the correct names and build targets
+                  const packageJson = editJsonFile(
+                    join(outputDirectory, "package.json")
+                  );
+                  packageJson.set("name", `@picovoice/${buildTarget}`);
+                  packageJson.save((e) => {
+                    console.log(`${buildTarget} Package JSON updated`);
+                  });
                 }
-              );
-            }
+              }
+            );
           }
-        );
+        });
       }
     });
   }
