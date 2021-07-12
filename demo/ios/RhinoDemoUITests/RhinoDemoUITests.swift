80cef53f18bf9ab062e35d9139f927f4091c437b
Ian Lavery <ian@picovoice.ai>
2021-07-12 11:52:36 -0700

    initial ios test framework

---
+++ demo/ios/RhinoDemoUITests/RhinoDemoUITests.swift
@@ -0,0 +1,24 @@
+import AVFoundation
+import XCTest
+
+import Rhino
+
+class RhinoDemoUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+
+    override func tearDown() {
+       super.tearDown()
+   }
+    
+    func testProcSuccessWithinContext() throws {
+
+    }
+    
+    func testProcFailOutOfContext() throws {
+
+    }
+}
