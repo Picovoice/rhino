21862323372f337fdf5e58870cbd8c595434f7c4
Ian Lavery <ian@picovoice.ai>
2021-11-19 16:54:35 -0800

    go demo arg change

--- demo/go/micdemo/rhino_mic_demo.go
+++ demo/go/micdemo/rhino_mic_demo.go
@@ -31,7 +31,7 @@ func main() {
 		"The value should be a number within [0, 1]. A higher sensitivity value results in "+
 		"fewer misses at the cost of (potentially) increasing the erroneous inference rate. "+
 		"If not set, 0.5 will be used.")
-	requireEndpointArg := flag.Bool("require_endpoint", false,
+	requireEndpointArg := flag.String("require_endpoint", "true",
 		"If set to `true`, Rhino requires an endpoint (chunk of silence) before finishing inference.")
 	audioDeviceIndex := flag.Int("audio_device_index", -1, "Index of capture device to use.")
 	outputPathArg := flag.String("output_path", "", "Path to recorded audio (for debugging)")
@@ -57,9 +57,11 @@ func main() {
 	}
 
 	r := rhino.Rhino{
-		RequireEndpoint: *requireEndpointArg,
+		RequireEndpoint: true,
 	}
-
+	if *requireEndpointArg == "false" {
+		r.RequireEndpoint = false
+	}
 	if *accessKeyArg == "" {
 		log.Fatalf("AccessKey is required.")
 	}
