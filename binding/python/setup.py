fa1b6ae113b4f2c1f64b1fa46dd053a18e565cff
Pouya Rostam <pouya@picovoice.ai>
2022-05-12 12:39:13 -0700

    released python packages

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -45,7 +45,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvrhino",
-    version="2.1.2",
+    version="2.1.3",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Rhino Speech-to-Intent engine.",
