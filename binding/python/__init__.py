1a4411fdae35de9fb49e459ea5a21f365691352a
Alireza Kenarsari <alireza@picovoice.ai>
2022-06-10 16:38:13 -0700

    wip

--- binding/python/__init__.py
+++ binding/python/__init__.py
@@ -29,7 +29,14 @@ LIBRARY_PATH = pv_library_path('')
 MODEL_PATH = pv_model_path('')
 
 
-def create(access_key, context_path, library_path=None, model_path=None, sensitivity=0.5, require_endpoint=True):
+def create(
+        access_key,
+        context_path,
+        library_path=None,
+        model_path=None,
+        sensitivity=0.5,
+        endpoint_duration_sec=1.,
+        require_endpoint=True):
     """
     Factory method for Rhino Speech-to-Intent engine.
 
@@ -43,8 +50,14 @@ def create(access_key, context_path, library_path=None, model_path=None, sensiti
     location.
     :param sensitivity: Inference sensitivity. It should be a number within [0, 1]. A higher sensitivity value
     results in fewer misses at the cost of (potentially) increasing the erroneous inference rate.
-    :param require_endpoint If set to `False`, Rhino does not require an endpoint (chunk of silence) before
-        finishing inference.
+    :param endpoint_duration_sec: Endpoint duration in seconds. An endpoint is a chunk of silence at the end of an
+    utterance that marks the end of spoken command. It should be a positive number within [0.5, 5]. A lower endpoint
+    duration reduces delay and improves responsiveness. A higher endpoint duration assures Rhino doesn't return
+    inference pre-emptively in case the user pauses before finishing the request.
+    :param require_endpoint: If set to `True`, Rhino requires an endpoint (a chunk of silence) after the spoken command.
+    If set to `False`, Rhino tries to detect silence, but if it cannot, it still will provide inference regardless.
+    Set to `False` only if operating in an environment with overlapping speech (e.g. people talking in the
+    background).
     :return An instance of Rhino Speech-to-Intent engine.
     """
 
@@ -60,4 +73,5 @@ def create(access_key, context_path, library_path=None, model_path=None, sensiti
         model_path=model_path,
         context_path=context_path,
         sensitivity=sensitivity,
+        endpoint_duration_sec=endpoint_duration_sec,
         require_endpoint=require_endpoint)
