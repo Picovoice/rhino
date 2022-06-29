960eb3288dbd4d6f5dd895d4864e492fc0d6fc50
Ian Lavery <ian@picovoice.ai>
2022-06-29 13:08:01 -0700

    Android language update (#396)

--- demo/android/Activity/copy_test_resources.sh
+++ demo/android/Activity/copy_test_resources.sh
@@ -5,8 +5,7 @@ then
 fi
 
 echo "Copying test audio samples..."
-cp ../../../resources/audio_samples/test_within_context.wav ./rhino-activity-demo-app/src/androidTest/assets/test_resources/audio_samples/test_within_context.wav
-cp ../../../resources/audio_samples/test_out_of_context.wav ./rhino-activity-demo-app/src/androidTest/assets/test_resources/audio_samples/test_out_of_context.wav
+cp ../../../resources/audio_samples/*.wav ./rhino-activity-demo-app/src/androidTest/assets/test_resources/audio_samples
 
 if [ ! -d "./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files" ]
 then 
@@ -15,12 +14,21 @@ then
 fi
 
 echo "Copying test model files..."
-cp ../../../lib/common/rhino_params.pv ./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files/rhino_params.pv
-cp ../../../lib/common/rhino_params_de.pv ./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files/rhino_params_de.pv
-cp ../../../lib/common/rhino_params_es.pv ./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files/rhino_params_es.pv
-cp ../../../lib/common/rhino_params_fr.pv ./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files/rhino_params_fr.pv
+cp ../../../lib/common/*.pv ./rhino-activity-demo-app/src/androidTest/assets/test_resources/model_files
+
+if [ ! -d "./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files" ]
+then
+    echo "Creating test context files directory..."
+    mkdir -p ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files
+fi
 
 echo "Copying test context files..."
 cp ../../../resources/contexts/android/coffee_maker_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/coffee_maker_android.rhn
 cp ../../../resources/contexts/linux/coffee_maker_linux.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/coffee_maker_linux.rhn
+cp ../../../resources/contexts_de/android/beleuchtung_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/beleuchtung_android.rhn
 cp ../../../resources/contexts_es/android/iluminación_inteligente_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/iluminación_inteligente_android.rhn
+cp ../../../resources/contexts_fr/android/éclairage_intelligent_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/éclairage_intelligent_android.rhn
+cp ../../../resources/contexts_it/android/illuminazione_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/illuminazione_android.rhn
+cp ../../../resources/contexts_ja/android/sumāto_shōmei_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/sumāto_shōmei_android.rhn
+cp ../../../resources/contexts_ko/android/seumateu_jomyeong_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/seumateu_jomyeong_android.rhn
+cp ../../../resources/contexts_pt/android/luz_inteligente_android.rhn ./rhino-activity-demo-app/src/androidTest/assets/test_resources/context_files/luz_inteligente_android.rhn
---
+++ demo/android/Activity/rhino-activity-demo-app/src/androidTest/java/ai/picovoice/rhinodemo/BaseTest.java
@@ -0,0 +1,115 @@
+package ai.picovoice.rhinodemo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import ai.picovoice.rhino.Rhino;
+import ai.picovoice.rhino.RhinoInference;
+
+public class BaseTest {
+    
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+    Context testContext;
+    Context appContext;
+    AssetManager assetManager;
+    String testResourcesPath;
+    String accessKey;
+
+    @After
+    public void TearDown() {
+        reportHelper.label("Stopping App");
+    }
+
+    @Before
+    public void Setup() throws IOException {
+        testContext = InstrumentationRegistry.getInstrumentation().getContext();
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assetManager = testContext.getAssets();
+        extractAssetsRecursively("test_resources");
+        testResourcesPath = new File(appContext.getFilesDir(), "test_resources").getAbsolutePath();
+
+        accessKey = appContext.getString(R.string.pvTestingAccessKey);
+    }
+
+    private void extractAssetsRecursively(String path) throws IOException {
+        String[] list = assetManager.list(path);
+        if (list.length > 0) {
+            File outputFile = new File(appContext.getFilesDir(), path);
+            if (!outputFile.exists()) {
+                if (!outputFile.mkdirs()) {
+                    throw new IOException("Couldn't create output directory " + outputFile.getAbsolutePath());
+                }
+            }
+
+            for (String file : list) {
+                String filepath = path + "/" + file;
+                extractAssetsRecursively(filepath);
+            }
+        } else {
+            extractTestFile(path);
+        }
+    }
+
+    private void extractTestFile(String filepath) throws IOException {
+        InputStream is = new BufferedInputStream(assetManager.open(filepath), 256);
+        File absPath = new File(appContext.getFilesDir(), filepath);
+        OutputStream os = new BufferedOutputStream(new FileOutputStream(absPath), 256);
+        int r;
+        while ((r = is.read()) != -1) {
+            os.write(r);
+        }
+        os.flush();
+
+        is.close();
+        os.close();
+    }
+
+    RhinoInference processTestAudio(Rhino r, File testAudio) throws Exception {
+        FileInputStream audioInputStream = new FileInputStream(testAudio);
+
+        byte[] rawData = new byte[r.getFrameLength() * 2];
+        short[] pcm = new short[r.getFrameLength()];
+        ByteBuffer pcmBuff = ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN);
+
+        assertEquals(44, audioInputStream.skip(44));
+
+        boolean isFinalized = false;
+        while (audioInputStream.available() > 0) {
+            int numRead = audioInputStream.read(pcmBuff.array());
+            if (numRead == r.getFrameLength() * 2) {
+                pcmBuff.asShortBuffer().get(pcm);
+                isFinalized = r.process(pcm);
+                if (isFinalized) {
+                    break;
+                }
+            }
+        }
+        assertTrue(isFinalized);
+
+        return r.getInference();
+    }
+}
