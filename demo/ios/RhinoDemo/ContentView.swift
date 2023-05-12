e82ea3214ba9b4972363a304344e9bb9cf3a2425
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-05-12 10:58:02 -0700

    update package and contextInfo

--- demo/ios/RhinoDemo/ContentView.swift
+++ demo/ios/RhinoDemo/ContentView.swift
@@ -14,7 +14,11 @@ struct SheetView: View {
     @Binding var contextInfo: String
 
     var body: some View {
-        Text(self.contextInfo)
+        ScrollView {
+            Text(self.contextInfo)
+                .padding()
+                .font(.system(size: 14))
+        }
     }
 }
 
@@ -32,6 +36,60 @@ struct ContentView: View {
 
     let ACCESS_KEY = "${YOUR_ACCESS_KEY_HERE}" // Obtained from Picovoice Console (https://console.picovoice.ai)
 
+    func initRhino() {
+        let contextPath = Bundle.main.url(
+            forResource: "\(context)_ios",
+            withExtension: "rhn",
+            subdirectory: "contexts")!.path
+
+        let modelPath = (language == "en") ? nil :
+            Bundle.main.url(
+                forResource: "rhino_params_\(language)",
+                withExtension: "pv",
+                subdirectory: "models")!.path
+        
+        do {
+            self.rhinoManager = try RhinoManager(
+                accessKey: self.ACCESS_KEY,
+                contextPath: contextPath,
+                modelPath: modelPath,
+                onInferenceCallback: { x in
+                    DispatchQueue.main.async {
+                        result = "{\n"
+                        self.result += "    \"isUnderstood\" : \"" +
+                        x.isUnderstood.description + "\",\n"
+                        if x.isUnderstood {
+                            self.result += "    \"intent : \"" + x.intent + "\",\n"
+                            if !x.slots.isEmpty {
+                                result += "    \"slots\" : {\n"
+                                for (k, v) in x.slots {
+                                    self.result += "        \"" + k + "\" : \"" + v + "\",\n"
+                                }
+                                result += "    }\n"
+                            }
+                        }
+                        result += "}\n"
+                        
+                        self.buttonLabel = "START"
+                    }
+                })
+            self.contextInfo = self.rhinoManager.contextInfo
+        } catch let error as RhinoInvalidArgumentError {
+            errorMessage =
+                "\(error.localizedDescription)\nEnsure your AccessKey '\(ACCESS_KEY)' is valid"
+        } catch is RhinoActivationError {
+            errorMessage = "ACCESS_KEY activation error"
+        } catch is RhinoActivationRefusedError {
+            errorMessage = "ACCESS_KEY activation refused"
+        } catch is RhinoActivationLimitError {
+            errorMessage = "ACCESS_KEY reached its limit"
+        } catch is RhinoActivationThrottledError {
+            errorMessage = "ACCESS_KEY is throttled"
+        } catch {
+            errorMessage = "\(error)"
+        }
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -54,56 +112,12 @@ struct ContentView: View {
                     if self.buttonLabel == "START" {
                         self.result = ""
 
-                        let contextPath = Bundle.main.url(
-                            forResource: "\(context)_ios",
-                            withExtension: "rhn",
-                            subdirectory: "contexts")!.path
-
-                        let modelPath = (language == "en") ? nil :
-                            Bundle.main.url(
-                                forResource: "rhino_params_\(language)",
-                                withExtension: "pv",
-                                subdirectory: "models")!.path
-
                         do {
-                            self.rhinoManager = try RhinoManager(
-                                accessKey: self.ACCESS_KEY,
-                                contextPath: contextPath,
-                                modelPath: modelPath,
-                                onInferenceCallback: { x in
-                                    DispatchQueue.main.async {
-                                        result = "{\n"
-                                        self.result += "    \"isUnderstood\" : \"" +
-                                            x.isUnderstood.description + "\",\n"
-                                        if x.isUnderstood {
-                                            self.result += "    \"intent : \"" + x.intent + "\",\n"
-                                            if !x.slots.isEmpty {
-                                                result += "    \"slots\" : {\n"
-                                                for (k, v) in x.slots {
-                                                    self.result += "        \"" + k + "\" : \"" + v + "\",\n"
-                                                }
-                                                result += "    }\n"
-                                            }
-                                        }
-                                        result += "}\n"
-
-                                        self.buttonLabel = "START"
-                                    }
-                                })
-                            self.contextInfo = ""
+                            if (self.rhinoManager == nil) {
+                                initRhino()
+                            }
                             try self.rhinoManager.process()
                             self.buttonLabel = "    ...    "
-                        } catch let error as RhinoInvalidArgumentError {
-                            errorMessage =
-                                "\(error.localizedDescription)\nEnsure your AccessKey '\(ACCESS_KEY)' is valid"
-                        } catch is RhinoActivationError {
-                            errorMessage = "ACCESS_KEY activation error"
-                        } catch is RhinoActivationRefusedError {
-                            errorMessage = "ACCESS_KEY activation refused"
-                        } catch is RhinoActivationLimitError {
-                            errorMessage = "ACCESS_KEY reached its limit"
-                        } catch is RhinoActivationThrottledError {
-                            errorMessage = "ACCESS_KEY is throttled"
                         } catch {
                             errorMessage = "\(error)"
                         }
@@ -124,6 +138,9 @@ struct ContentView: View {
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
             .background(Color.white)
             .navigationBarItems(trailing: Button("Context Info") {
+                if (self.rhinoManager == nil) {
+                    initRhino()
+                }
                 self.showInfo = true
             })
         }
