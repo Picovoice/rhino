20df711dd57c72cfc8504853daf4a64cca5f9b40
Matthew Maxwell <matthew@picovoice.ai>
2024-06-27 12:23:53 -0700

    applied dart fix and updated flutter versions

--- demo/flutter/lib/main.dart
+++ demo/flutter/lib/main.dart
@@ -200,7 +200,7 @@ class _MyAppState extends State<MyApp> {
 
   buildContextHeader(BuildContext context) {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        primary: picoBlue, textStyle: TextStyle(color: Colors.white));
+        backgroundColor: picoBlue, textStyle: TextStyle(color: Colors.white));
 
     return Expanded(
         flex: 1,
@@ -231,7 +231,7 @@ class _MyAppState extends State<MyApp> {
 
   buildStartButton(BuildContext context) {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        primary: picoBlue,
+        backgroundColor: picoBlue,
         shape: CircleBorder(),
         textStyle: TextStyle(color: Colors.white));
 
